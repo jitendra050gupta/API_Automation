b5e371612fcd3b571dcd3200ce5b1a4e4c7ab1cc
kumarguptaj <kumarguptaj@LP-5CD7490BPC.HCLT.CORP.HCL.IN>
2020-01-23 16:38:04 +0530

    First API Commit

---
+++ CommerceAPIs/src/test/java/WeatherAPIsTest/GetAPITest.java
@@ -0,0 +1,56 @@
+package WeatherAPIsTest;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import junit.framework.Assert;
+
+public class GetAPITest {
+	
+	@Test
+	public void statusCodeTest() {
+		
+		RestAssured.baseURI = "http://jsonplaceholder.typicode.com/posts";
+		
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.request(Method.GET, "/3");
+		
+		String body = response.body().asString();
+		// -------------Printing Response body------------
+		System.out.println(body);
+		
+		// ------------Verifying Status code of the response-----------
+		System.out.println("Status code is "+response.getStatusCode());
+		Assert.assertEquals(response.statusCode(), 200);
+		
+		// -----------Verifying Status line of the response-------------
+		System.out.println("Status Line is "+response.statusLine());
+		Assert.assertEquals(response.statusLine(), "HTTP/1.1 200 OK");
+		
+		//-------Verifying attribute value from the response body---------
+		System.out.println("Id in response body is ---> "+response.jsonPath().get("id").toString());
+		
+		// ----------------Printing all headers----------		
+		System.out.println("----------All headers are ------------");
+		Headers allHeaders = response.headers();
+		for(Header header:allHeaders) {
+			System.out.println(header.getName()+"      "+header.getValue());			
+			
+		}
+		
+		// -------------Verifying header value------------
+		Assert.assertEquals(response.header("Content-Type"), "application/json; charset=utf-8");
+		
+		
+		
+	}
+	
+	
+	
+
+}
---
+++ CommerceAPIs/src/test/java/WeatherAPIsTest/PostAPITest.java
@@ -0,0 +1,55 @@
+package WeatherAPIsTest;
+
+import java.util.Random;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import junit.framework.Assert;
+
+public class PostAPITest {
+
+	@Test
+	public void createUserTest() {
+		RestAssured.baseURI = "http://restapi.demoqa.com/customer";
+		RequestSpecification httpRequest = RestAssured.given();
+
+		Random rand = new Random();
+		int num = rand.nextInt(100000);
+		String userName = num + "user";
+		String email = num + "@test.com";
+
+		// Creating JSON object to create request body in JSON
+		JSONObject jsonBody = new JSONObject();
+		jsonBody.put("FirstName", "Jitendra");
+		jsonBody.put("LastName", "Gupta");
+		jsonBody.put("UserName", userName);
+		jsonBody.put("Password", "12334566");
+		jsonBody.put("Email", email);
+
+		// Defining header type inside the header
+		httpRequest.header("Content-Type", "application/json");
+
+		// Adding request body into request specification object
+		httpRequest.body(jsonBody.toJSONString());
+
+		// hitting the request and storing the response into response object
+		Response rsponse = httpRequest.request(Method.POST, "/register");
+
+		// ---------Printing Response Body-----------
+		System.out.println("--------------Response Body ----------------");
+		System.out.println(rsponse.body().asString());
+
+		// ------------Verifying Status code of the response-----------
+		System.out.println("Status code is " + rsponse.getStatusCode());
+		Assert.assertEquals(rsponse.statusCode(), 201);
+
+		// ---------Verifying Response Body attributes-----------
+		Assert.assertEquals(rsponse.jsonPath().getString("SuccessCode"), "OPERATION_SUCCESS");
+
+	}
+
+}
